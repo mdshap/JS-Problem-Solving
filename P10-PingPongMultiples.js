c0f2defb1e2be49d99fc4792832baeb5b9231414
mdshap <notunmdshaptarshi@gmail.com>
2026-01-08 16:01:36 +0600

    all done

---
+++ P10-PingPongMultiples.js
@@ -0,0 +1,20 @@
+function multiples() {
+  for (let i = 1; i <= 20; i++) {
+    if (i % 3 === 0 && i % 5 !== 0){
+      console.log("Ping, ");
+    } 
+    else if (i % 5 === 0 && i % 3 !== 0) {
+        console.log("Pong, ");
+        
+    }
+    else if(i % 3 === 0 && i % 5 === 0){
+        console.log('PingPong');     
+    }
+    else{
+        console.log(i + ", ");
+        
+    }
+  }
+}
+
+multiples()
