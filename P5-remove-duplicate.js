9fd64f9465617a979ac209bbfc340d1a7c02edc8
mdshap <notunmdshaptarshi@gmail.com>
2026-01-08 15:27:58 +0600

    p5 solved

---
+++ P5-remove-duplicate.js
@@ -0,0 +1,11 @@
+const arr= [1,2,2,3,4,4]
+
+for(let i=0; i<arr.length; i++){
+    for(let j=i+1; j<arr.length; j++){
+        if(arr[i]===arr[j]){
+            arr.splice(j,1);
+        }
+    }
+}
+
+console.log(arr)
