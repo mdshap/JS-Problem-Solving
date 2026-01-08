23d159b36a5d72bf8c9a319f46300dfa1af20690
mdshap <notunmdshaptarshi@gmail.com>
2026-01-08 15:08:14 +0600

    p1 solved

---
+++ P1-Reverse-a-String.js
@@ -0,0 +1,9 @@
+const str = "hello";
+
+let reversed = "";
+
+for(let i= str.length -1; i>=0; i--){
+    reverse = reverse + str[i]
+}
+
+console.log(reverse)
---
+++ P1_Reverse_a_string.js
@@ -0,0 +1,9 @@
+let str = "hello";
+
+let reversed ="";
+
+for(let i = str.length-1; i>=0; i--){
+    reversed = reversed + str[i]
+}
+
+console.log(reversed)
