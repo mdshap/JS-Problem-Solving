36c46a2fab7c4d5c4e23679659b1eac563f6ae4e
mdshap <notunmdshaptarshi@gmail.com>
2026-01-08 15:18:47 +0600

    p3 solved

---
+++ P3-String-Palindrome.js
@@ -0,0 +1,13 @@
+const str= "madam";
+let reversed = "";
+
+for(let i = str.length-1; i>=0; i--){
+    reversed = reversed + str[i]
+}
+
+if(str == reversed){
+    console.log(true);   
+}
+else{
+    console.log(false);  
+};
